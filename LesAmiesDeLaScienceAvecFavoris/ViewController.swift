75494da0471ec7d071a6dad525ef6f402ed3f8d4
Alain Boudreault <alain.boudreault@me.com>
2017-11-26 12:42:16 -0500

    Initial Commit

---
+++ LesAmiesDeLaScienceAvecFavoris/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LesAmiesDeLaScienceAvecFavoris
+//
+//  Created by Alain on 17-11-26.
+//  Copyright © 2017 Alain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
